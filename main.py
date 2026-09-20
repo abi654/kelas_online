a72d61f6bc448688d9da10ddb81dceaa2697cef3
abi654 <154344153+abi654@users.noreply.github.com>
2026-09-20 16:37:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+print("hello world") nama = "abbie"
+>>> hobi = "bola"
+>>> 
+print(nama)
+abbie
+>>> print(hobi)
+bola
+nama = "abbie_muajhiddin"
+>>> print(nama
+nama = "abbie"
+hobi = "ngoding"
+print(nama)
+print(hobi)
+a = 1.53
+b = 7.2
+c = a + b
+print(a + b)
+abbie
+teks = "Rahmat Flowchart"
+
+print()
+Hujan = False
+Panas = True
+mata_kuliah >= ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2])
+
+mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2])
+nama  = input("abbie: ")
+print("Nama saya adalah", nama)
+
+
+
+
+
